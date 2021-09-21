9111af6b652143bc9d90be5395fcbdd0fcd3ca10
hrithwik6986 <4ni17is028_a@nie.ac.in>
2021-09-21 14:05:34 +0530

    first commit

---
+++ _next/static/t7PEGzSsfhe4LUAw8lSUZ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[slug]","\u002F[slug]\u002F[...path]","\u002Fdomain\u002F[custom_domain]\u002F[...path]","\u002Fdomain\u002F[custom_domain]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
